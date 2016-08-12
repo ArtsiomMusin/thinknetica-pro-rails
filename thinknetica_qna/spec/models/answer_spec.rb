6e7aa53fee80c1588108e40c3ba40cfe1c8a87bc
Artsiom <musin@us.ibm.com>
2016-08-12 13:52:54 +0300

    Merge removed changes from master

---
+++ thinknetica_qna/spec/models/answer_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Answer, type: :model do
+  it 'validates presence of body' do
+    expect(Answer.new).to_not be_valid
+  end
+
+  it { should belong_to(:question) }
+end
