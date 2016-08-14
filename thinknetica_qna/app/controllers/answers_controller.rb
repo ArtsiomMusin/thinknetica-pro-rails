9372e0d0b57b891ea264d195f646194e01bb8575
Artsiom Musin <artyom.musin@gmail.com>
2016-08-14 23:49:04 +0300

    Some incomplete tests for answers

--- thinknetica_qna/app/controllers/answers_controller.rb
+++ thinknetica_qna/app/controllers/answers_controller.rb
@@ -1,2 +1,27 @@
 class AnswersController < ApplicationController
+  def index
+    @answers = Answer.all
+  end
+
+  def show
+    @answer = Answer.find params[:id]
+  end
+
+  def new
+    @answer = Answer.new
+  end
+
+  def create
+    @answer = Answer.new(answer_params)
+    if @answer.save
+      redirect_to @answer
+    else
+      render :new
+    end
+  end
+
+  private
+  def answer_params
+    params.require(:answer).permit(:body, :question)
+  end
 end
--- thinknetica_qna/spec/controllers/answers_controller_spec.rb
+++ thinknetica_qna/spec/controllers/answers_controller_spec.rb
@@ -1,5 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  let(:answer) { create(:answer) }
+  describe 'GET #index' do
+    let(:answers) { create_list(:answer, 2) }
+    before { get :index }
+    it 'gets all answers' do
+      expect(assigns(:answers)).to match_array(answers)
+    end
+    it 'renders index' do
+      expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+    before { get :show, id: answer }
+    it 'shows one specific answer' do
+      expect(assigns(:answer)).to eq answer
+    end
+    it 'renders index' do
+      expect(response).to render_template :show
+    end
+  end
+
+  describe 'GET #new' do
+    before { get :new }
+    it 'creates a new answer' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+    it 'renders new' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'POST #create' do
+    context 'valid tests' do
+      let(:question) { create(:question) }
+      it 'creates a new answer with parameters' do
+        expect { post :create, answer: attributes_for(:answer) }.to change(Answer, :count).by(1)
+      end
+      it 'renders show after creating a new answer' do
+        post :create, answer: attributes_for(:answer)
+        expect(response).to redirect_to answer_path(assigns(:answer))
+      end
+    end
+    context 'invalid tests' do
+      it 'fails with an incomplete answer' do
+        expect { post :create, answer: attributes_for(:invalid_answer) }.to_not change(Answer, :count)
+      end
+
+      it 'renders new again' do
+        post :create, answer: attributes_for(:invalid_answer)
+        expect(response).to render_template :new
+      end
+    end
+  end
 
 end
