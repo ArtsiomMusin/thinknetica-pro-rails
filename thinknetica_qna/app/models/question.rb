fd4e18829e27d1c12d6971e8fedd97a18c371292
Artsiom <musin@us.ibm.com>
2016-08-15 15:17:46 +0300

    Test adding answers to question

--- thinknetica_qna/app/models/question.rb
+++ thinknetica_qna/app/models/question.rb
@@ -2,4 +2,8 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
 
   validates :title, :body, presence: true
+
+  def add_answer(answer)
+    answers.push(answer)
+  end
 end
--- thinknetica_qna/spec/models/question_spec.rb
+++ thinknetica_qna/spec/models/question_spec.rb
@@ -4,4 +4,13 @@ RSpec.describe Question, type: :model do
   it { should have_many(:answers).dependent(:destroy) }
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:body) }
+
+  describe 'test methods' do
+    let(:question) { create(:question) }
+    let(:answer) { create(:answer) }
+    it 'adds answers to a question' do
+      question.add_answer(answer)
+      expect(question.answers.first).to eq answer
+    end
+  end
 end
