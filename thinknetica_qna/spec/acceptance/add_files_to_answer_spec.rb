69008612b1bc46b7564bfb9192d1eb585a4eeceb
Artsiom Musin <artyom.musin@gmail.com>
2016-09-08 00:59:19 +0300

    More acceptance scenarios

--- thinknetica_qna/spec/acceptance/add_files_to_answer_spec.rb
+++ thinknetica_qna/spec/acceptance/add_files_to_answer_spec.rb
@@ -5,7 +5,10 @@ feature 'Add files to answer', %q{
   As an authenticated user
   I want to be able to attach files
 } do
+  given(:user) { create(:user) }
   given(:question) { create(:question) }
+  given(:answer) { create(:answer, question: question)}
+  given(:attachment) { create(:attachment, attachable: answer) }
   scenario 'Authenticated user adds when adds an answer', js: true do
     sign_in(question.user)
 
@@ -15,4 +18,29 @@ feature 'Add files to answer', %q{
     click_on 'Add answer'
     expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
   end
+
+  scenario 'Authenticated user adds a couple of attachments when adds an answer', js: true do
+    sign_in(question.user)
+
+    visit question_path(question)
+    fill_in 'Body', with: 'Body text'
+    click_link 'Add Attachment'
+    inputs = page.all('input[type=file]')
+    inputs[0].set("#{Rails.root}/spec/spec_helper.rb")
+    inputs[1].set("#{Rails.root}/spec/rails_helper.rb")
+    click_on 'Add answer'
+
+    expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+    expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
+  end
+
+  scenario 'Another user can see attachments of another user', js: true do
+    answer.attachments << attachment
+    sign_in(question.user)
+    visit question_path(question)
+    expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+  end
+
+  scenario 'Author deletes an attachment'
+  scenario 'Another user cannot delete an attachment belongs to another user'
 end
--- thinknetica_qna/spec/acceptance/add_files_to_question_spec.rb
+++ thinknetica_qna/spec/acceptance/add_files_to_question_spec.rb
@@ -6,8 +6,9 @@ feature 'Add files to question', %q{
   I want to be able to attach files
 } do
   given(:user) { create(:user) }
-
-  scenario 'Authenticated user adds an attachment when asks a question' do
+  given(:question) { create(:question) }
+  given(:attachment) { create(:attachment, attachable: question) }
+  scenario 'Authenticated user adds an attachment when asks a question', js: true do
     sign_in(user)
 
     visit new_question_path
@@ -30,8 +31,18 @@ feature 'Add files to question', %q{
     inputs[0].set("#{Rails.root}/spec/spec_helper.rb")
     inputs[1].set("#{Rails.root}/spec/rails_helper.rb")
     click_on 'Create'
-    
-    expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/2/spec_helper.rb'
-    expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/3/rails_helper.rb'
+
+    expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+    expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
   end
+
+  scenario 'Another user can see attachments of another user', js: true do
+    question.attachments << attachment
+    sign_in(question.user)
+    visit question_path(question)
+    expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+  end
+
+  scenario 'Author deletes an attachment'
+  scenario 'Another user cannot delete an attachment belongs to another user'
 end
