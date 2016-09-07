252b066a1fc12eba7c3b03f1e2fe11c32b0a774f
Archik <artyom.musin@gmail.com>
2016-09-07 23:29:59 +0300

    Add multiple attachments for questions
    * Try out multiple attachments
    
    * Add cocoon stuff
    
    * Fixing test errors

---
+++ thinknetica_qna/spec/acceptance/add_files_to_question_spec.rb
@@ -0,0 +1,37 @@
+require_relative 'acceptance_helper'
+
+feature 'Add files to question', %q{
+  In order to illustrate the question
+  As an authenticated user
+  I want to be able to attach files
+} do
+  given(:user) { create(:user) }
+
+  scenario 'Authenticated user adds an attachment when asks a question' do
+    sign_in(user)
+
+    visit new_question_path
+    fill_in 'Title', with: 'Test Title'
+    fill_in 'Body', with: 'Question text'
+    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    click_on 'Create'
+
+    expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+  end
+
+  scenario 'Authenticated user adds a couple of attachments when asks a question', js: true do
+    sign_in(user)
+
+    visit new_question_path
+    fill_in 'Title', with: 'Test Title'
+    fill_in 'Body', with: 'Question text'
+    click_link 'Add Attachment'
+    inputs = page.all('input[type=file]')
+    inputs[0].set("#{Rails.root}/spec/spec_helper.rb")
+    inputs[1].set("#{Rails.root}/spec/rails_helper.rb")
+    click_on 'Create'
+    
+    expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/2/spec_helper.rb'
+    expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/3/rails_helper.rb'
+  end
+end
