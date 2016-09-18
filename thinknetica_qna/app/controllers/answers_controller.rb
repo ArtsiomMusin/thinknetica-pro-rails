8925776eea9fa94e36833b81eb951416a44328f4
Artsiom Musin <artyom.musin@gmail.com>
2016-09-18 23:32:45 +0300

    Use responders for all controllers

--- thinknetica_qna/app/controllers/answers_controller.rb
+++ thinknetica_qna/app/controllers/answers_controller.rb
@@ -1,33 +1,29 @@
 class AnswersController < ApplicationController
   before_action :authenticate_user!
-  before_action :load_answer, only: [:update, :destroy, :mark_best]
+  before_action :load_answer, except: :create
+  before_action :load_question, only: :create
+  before_action :load_question_from_answer, only: :mark_best
 
   include Voted
+  respond_to :js
 
   def create
-    @question = Question.find(params[:question_id])
-    @answer = @question.answers.create(answer_params.merge(user: current_user))
-    # respond_to do |format|
-    #   if @answer.save
-    #     format.json { render json: @answer }
-    #   else
-    #     format.json {
-    #       render json: @answer.errors.full_messages, status: :unprocessable_entity }
-    #   end
-    # end
+    respond_with(@answer = @question.answers.create(answer_params.merge(user: current_user)))
   end
 
   def update
-    @answer.update(answer_params) if current_user.author_of?(@answer)
+    if current_user.author_of?(@answer)
+      @answer.update(answer_params)
+      respond_with @answer
+    end
   end
 
   def destroy
-    @answer.destroy! if current_user.author_of?(@answer)
+    respond_with(@answer.destroy!) if current_user.author_of?(@answer)
   end
 
   def mark_best
-    @question = @answer.question
-    @answer.make_best if current_user.author_of?(@question)
+    respond_with(@answer.make_best) if current_user.author_of?(@question)
   end
 
   private
@@ -37,5 +33,13 @@ class AnswersController < ApplicationController
 
   def load_answer
     @answer = Answer.find(params[:id])
+  end
+
+  def load_question
+    @question = Question.find(params[:question_id])
+  end
+
+  def load_question_from_answer
+    @question = @answer.question
   end
 end
--- thinknetica_qna/app/controllers/questions_controller.rb
+++ thinknetica_qna/app/controllers/questions_controller.rb
@@ -1,43 +1,41 @@
 class QuestionsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :load_question, only: [:show, :update, :destroy]
+  before_action :create_answer, only: :show
+  after_action :publish_question, only: :create
 
   include Voted
+  respond_to :js, only: [:update]
 
   def index
-    @questions = Question.all
+    respond_with(@questions = Question.all)
   end
 
   def show
-    @comment = @question.comments.build
-    @answer = Answer.new
-    @answer.attachments.build
-    @answer.comments.build
+    respond_with @question
   end
 
   def new
-    @question = Question.new
-    @question.attachments.build
+    respond_with(@question = Question.new)
   end
 
   def create
-    @question = current_user.questions.new(question_params)
-    if @question.save
-      PrivatePub.publish_to "/questions", question: @question.to_json
-      redirect_to @question, notice: 'Your question created successfully.'
-    else
-      flash[:notice] = 'Could not create a question.'
-      render :new
-    end
+    respond_with(@question = current_user.questions.create(question_params))
   end
 
   def update
-    @question.update(question_params) if current_user.author_of?(@question)
+    if current_user.author_of?(@question)
+      @question.update(question_params)
+      respond_with @question
+    end
   end
 
   def destroy
-    @question.destroy! if current_user.author_of?(@question)
-    redirect_to root_path, notice: 'Question removed successfully.'
+    if current_user.author_of?(@question)
+      respond_with(@question.destroy!) do |format|
+        format.html { redirect_to root_path }
+      end
+    end
   end
 
   private
@@ -45,8 +43,15 @@ class QuestionsController < ApplicationController
     params.require(:question).permit(:title, :body, attachments_attributes: [:id, :_destroy, :file])
   end
 
+  def create_answer
+    @answer = Answer.new
+  end
+
   def load_question
     @question = Question.find(params[:id])
   end
 
+  def publish_question
+    PrivatePub.publish_to "/questions", question: @question.to_json if @question.valid?
+  end
 end
