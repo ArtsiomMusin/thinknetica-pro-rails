8f3fea5f18febda125f4d1558e43c6e1534e44c8
Artsiom Musin <artyom.musin@gmail.com>
2016-09-20 23:53:03 +0300

    Add confitmable for devise

--- thinknetica_qna/db/schema.rb
+++ thinknetica_qna/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160920095452) do
+ActiveRecord::Schema.define(version: 20160920192850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160920095452) do
     t.integer  "question_id"
     t.integer  "user_id"
     t.boolean  "best",        default: false
+    t.index ["question_id"], name: "index_answers_on_question_id", using: :btree
     t.index ["user_id"], name: "index_answers_on_user_id", using: :btree
   end
 
@@ -58,7 +59,6 @@ ActiveRecord::Schema.define(version: 20160920095452) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "answer_id"
     t.integer  "user_id"
     t.index ["user_id"], name: "index_questions_on_user_id", using: :btree
   end
@@ -76,6 +76,10 @@ ActiveRecord::Schema.define(version: 20160920095452) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
@@ -91,6 +95,7 @@ ActiveRecord::Schema.define(version: 20160920095452) do
     t.index ["votable_id", "votable_type"], name: "index_votes_on_votable_id_and_votable_type", using: :btree
   end
 
+  add_foreign_key "answers", "questions"
   add_foreign_key "answers", "users"
   add_foreign_key "authorizations", "users"
   add_foreign_key "questions", "users"
