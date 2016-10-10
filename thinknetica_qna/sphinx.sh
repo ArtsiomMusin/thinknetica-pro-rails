680f1689e75339cd9f2aab192f3915a80bb358fa
ArtsiomMusin <artyom.musin@gmail.com>
2016-10-10 14:00:16 +0300

    More travis changes
    More travis changes
    
    Trying out to start sphinx properly
    
    Try to rebuild
    
    Try to rebuild
    
    More changes

--- thinknetica_qna/sphinx.sh
+++ thinknetica_qna/sphinx.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+# sphinx on ubuntu
+# https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-sphinx-on-ubuntu-14-04
 
-rake ts:start
-rake ts:index
+bundle exec rake ts:configure
+bundle exec rake ts:start
+bundle exec rake ts:index
 
 # less security? yes!
 sudo /bin/sed -i 's/md5/trust/g' /etc/postgresql/9.3/main/pg_hba.conf
