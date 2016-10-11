37c9037f1ceeb1a64cc2f1b64ce7253dafb68e29
Artsiom Musin <artyom.musin@gmail.com>
2016-10-11 23:42:34 +0300

    Controller specs + indexes

--- thinknetica_qna/app/controllers/searches_controller.rb
+++ thinknetica_qna/app/controllers/searches_controller.rb
@@ -1,9 +1,15 @@
 class SearchesController < ApplicationController
   respond_to :js
 
-  #authorize_resource
+  authorize_resource class: false
 
   def show
-    respond_with(@results = Question.search(params[:text]))
+    @results = []
+    text = ThinkingSphinx::Query.escape params[:text]
+    %w(questions answers comments users).each do |filter|
+      @results.push(filter.classify.constantize.search(text)) if params[filter.to_sym]
+    end
+    @results = ThinkingSphinx.search(params[:text]) if @result.blank?
+    respond_with @results
   end
 end
--- thinknetica_qna/spec/controllers/searches_controller_spec.rb
+++ thinknetica_qna/spec/controllers/searches_controller_spec.rb
@@ -2,16 +2,29 @@ require 'rails_helper'
 
 RSpec.describe SearchesController, type: :controller do
   describe 'GET #show' do
-    context 'questions' do
-      let(:questions) { create_list(:question, 2, body: 'some body to find') }
-      it 'finds all questions' do
-        expect(Question).to receive(:search).with('some body to find')
-        get :show, text: 'some body to find', questions: true, format: :js
-      end
-      # it 'renders index' do
-      #   expect(response).to render_template :index
-      # end
+    let(:question) { create(:question, body: 'some body to find') }
+    let(:answer) { create(:answer, body: 'some body to find') }
+    let(:comment) { create(:comment, body: 'some body to find') }
+    let(:user) { create(:user, email: 'some@mail.ru') }
+    it 'finds all questions' do
+      expect(Question).to receive(:search).with('some body to find')
+      xhr :get, :show, text: 'some body to find', questions: true, format: :js
     end
-
+    it 'finds all answers' do
+      expect(Answer).to receive(:search).with('some body to find')
+      xhr :get, :show, text: 'some body to find', answers: true, format: :js
+    end
+    it 'finds all comments' do
+      expect(Comment).to receive(:search).with('some body to find')
+      xhr :get, :show, text: 'some body to find', comments: true, format: :js
+    end
+    it 'finds all users' do
+      expect(User).to receive(:search).with('some\\@mail.ru')
+      xhr :get, :show, text: 'some@mail.ru', users: true, format: :js
+    end
+    it 'finds everything' do
+      expect(ThinkingSphinx).to receive(:search).with('some')
+      xhr :get, :show, text: 'some', format: :js
+    end
   end
 end
